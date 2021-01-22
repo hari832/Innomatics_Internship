07bffa61aed0b7ebd58209fc06173bc01b925679
hari832 <74483620+hari832@users.noreply.github.com>
2021-01-22 12:40:32 +0530

    Add files via upload

---
+++ Day_3/1.Find_angle_MBC.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+ab=int(input())
+bc=int(input())
+MBC=round(math.degrees(math.atan(ab/bc)))
+print(str(MBC)+"°")
---
+++ Day_3/7.Polar_coordinates.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+x=complex(input())
+r=cmath.polar(x)
+print(r[0])
+print(r[1])
